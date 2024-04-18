ae2c2578c60a0f07edfcaefa2888a9931cd7244f
ajtollo <khesir2002@gmail.com>
2024-04-18 08:06:44 +0800

    Small fix on join

--- main.py
+++ main.py
@@ -44,8 +44,8 @@ class Ari(commands.Bot):
   
   async def on_guild_join(self,guild):
     print(f'Bot has been added to a new server {guild.name}')
-    guild = self.get_guild(939025934483357766)
-    target_log = guild.get_channel(1230069779071762473)
+    guildx = self.get_guild(939025934483357766)
+    target_log = guildx.get_channel(1230069779071762473)
     target_channel = guild.system_channel  # Use the system channel for the guild
     if target_channel is not None:  # Ensure there's a system channel
         await target_channel.send(f"💖 **Thank you for inviting {self.user.name}!!**\n\n__**A brief intro**__\nHey Everyone! My main purpose is creating an Inter Guild / Server Connectivity to bring the world closer together!\nHope you'll find my application useful! Thankyouuu~\n\nType `a!about` to know more about me and my usage!\n\n**__Servers Connected__**\n{len(self.guilds)}\n\n")
