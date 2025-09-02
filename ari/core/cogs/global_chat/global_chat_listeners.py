d1aac8d901290d8abf6314e1b0026c78a0d5f385
Khesir <khesir2002@gmail.com>
2025-09-02 23:49:19 +0800

    Update global_chat_listeners.py

--- ari/core/cogs/global_chat/global_chat_listeners.py
+++ ari/core/cogs/global_chat/global_chat_listeners.py
@@ -195,7 +195,7 @@ class EventListeners(commands.Cog):
 
     async def process_message(self, webhook : Webhook, message : discord.Message, messagesData, embed = None):
         try:
-            allowed_mentions = discord.AllowedMentions(everyone=False, users=False, roles=False)
+            allowed_mentions = discord.AllowedMentions(everyone=True, users=True, roles=True)
 
             files = [await attachment.to_file() for attachment in message.attachments]
             
